5324381524bdf75aabd150fc40cef6634ca94205
samx1118 <samx_48783_@hotmail.com>
2016-05-16 00:21:43 +0700

    Add app folder v0.1

---
+++ MagicBeginHere/main.lua
@@ -0,0 +1,49 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+-- Hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- Set Title
+local titleOption = {
+	text = "Tamagotchi v0.1",
+	x = display.contentCenterX,
+	y = 24,
+	fontSize = 24,
+	width = 200,
+	height = 0
+}
+local title = display.newText(titleOption)
+
+-- Set Background
+local background = display.newImage("background.png", display.contentCenterX, display.contentCenterY)
+
+
+-- Set Tamagotchi
+local tamagotchiOption = {
+	width = 100,
+	height = 98,
+	numFrames = 5,
+
+}
+local tamagotchi = graphics.newImageSheet("bird.png", tamagotchiOption)
+
+local sequence = {
+	{
+		name = "normal",
+		start = 1,
+		count = 5,
+		time = 1000,
+		loopcount = 0,
+		loopdirection = "forward"
+	}
+}
+local anime = display.newSprite(tamagotchi, sequence)
+anime.x = display.contentCenterX
+anime.y = 250
+anime:play()
